5fdff16770d68f0d402d2cfc062064923b866a72
Mohamed Amine <mohamedamine.soltana@tbs.u-tunis.tn>
2024-11-27 09:44:17 +0100

    swagger

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-from flask import Flask
 from flask_smorest import Api, Blueprint
 from flask import Flask, request, abort
 from resources.course_item import blp as Course_item_Blueprint
@@ -9,8 +8,8 @@ app.config["API_TITLE"]="TBS REST API"
 app.config["API_VERSION"]="RELEASE 1"
 app.config["OPENAPI_VERSION"]="3.0.3"
 app.config["OPENAPI_URL_PREFIX"]="/"
-app.config["OPENAPI_SWAGGER_UI_PATH"]="/swagger-ui"
-app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist"
+app.config["OPENAPI_SWAGGER_UI_PATH"]="/swagger-ui/"
+app.config["OPENAPI_SWAGGER_UI_URL"]="https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 api=Api(app)
 api.register_blueprint(Course_item_Blueprint)
 api.register_blueprint(Specialization_Blueprint)
--- env/resources/specialization.py
+++ env/resources/specialization.py
@@ -1,32 +1,43 @@
-import uuid
-from flask_smorest import Blueprint, Api
-from flask import abort, request
+from flask_smorest import Blueprint
 from flask.views import MethodView
 from db import specializations
-blp=Blueprint("specialization", __name__, description="operations")
+from flask import abort,request
+import uuid
+
+blp = Blueprint("specialization",__name__ , description="Operations on specialization" )
+
+
+@blp.route("/specialization/<string:specialization_id>")
 class Specialization(MethodView):
-    def get(self, specialization_id):
+    def get(self,specialization_id):
         try:
+    # Here you might also want to add the course_items in this specialization
+    # We'll do that later on in the course
             return specializations[specialization_id]
         except KeyError:
-            abort(404, description ="Specialization not found.")
-    def delete(self, specialization_id):
+            abort(404, message="Specialization not found.")
+    
+        
+    def delete(self,specialization_id):
         try:
             del specializations[specialization_id]
             return {"message": "Specialization deleted."}
         except KeyError:
-            abort(404, description ="Specialization not found.")
+            abort(404, message="Specialization not found.")
 
+
+@blp.route("/specialization")
 class SpecializationList(MethodView):
     def get(self):
-        return {"specializations": list(specializations.values())}
+        return {"specializations": list(specializations.values())} 
+
     def post(self):
         specialization_data = request.get_json()
         if "name" not in specialization_data:
-            abort(400,description ="Bad request. Ensure 'name' is included in the JSON payload.",)
+            abort(400,message="Bad request. Ensure 'name' is included in the JSON payload.",)
         for specialization in specializations.values():
             if specialization_data["name"] == specialization["name"]:
-                abort(400, description="Specialization already exists.")
+                abort(400, message="Specialization already exists.")
         specialization_id = uuid.uuid4().hex
         specialization = {**specialization_data, "id": specialization_id}
         specializations[specialization_id] = specialization
